e0aefb5eb92cd8f42ecfddcb336ea66885b9a3c5
윤성 <jeonpanda0705@gmail.com>
2026-09-19 19:01:34 +0900

    a

--- aYunseong/src/main/java/com/ys/ys_mcbg/BgMapConstants.java
+++
@@ -1,8 +0,0 @@
-package com.ys.ys_mcbg;
-
-public class BgMapConstants {
-    public static final int MAP_SIZE = 800;          // 800x800 블록
-    public static final int HALF = MAP_SIZE / 2;     // 400
-    public static final int CHUNKS = MAP_SIZE / 16;  // 50
-    public static final int MIN_CHUNK = -CHUNKS / 2; // -25
-}
--- aYunseong/src/main/java/com/ys/ys_mcbg/ClientBoostCache.java
+++
@@ -1,5 +0,0 @@
-package com.ys.ys_mcbg;
-
-public class ClientBoostCache {
-    public static float boost = 0f; // 0.0 ~ 1.0
-}
